7ffda0b8698abc1044851b1f1c7532a631b6a73d
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2019-08-06 12:29:53 +0900

    bundle gem open3

---
+++ test/open3_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+class Open3Test < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Open3::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
