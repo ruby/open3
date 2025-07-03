f976c2b765d6e90da800da5c4f326afaae6e81fa
Randy Stauner <randy@r4s6.net>
2025-07-03 08:43:53 -0700

    Generalize redirection option warnings in Open3 documentation
    Replace specific implementation details about which redirection options
    are ignored with a more general user-friendly message. This avoids
    exposing internal implementation details and provides clearer guidance
    to users.

--- lib/open3.rb
+++ lib/open3.rb
@@ -154,8 +154,8 @@ module Open3
   # in the call to Process.spawn;
   # see {Execution Options}[https://docs.ruby-lang.org/en/master/Process.html#module-Process-label-Execution+Options].
   #
-  # Note: The redirection options :in, :out, and :err will be ignored
-  # because popen3 sets up pipes for stdin, stdout, and stderr.
+  # Note: Redirection options are managed by the method and should not be
+  # provided as they may be ignored or lead to errors.
   #
   # The single required argument is one of the following:
   #
@@ -312,8 +312,8 @@ module Open3
   # in the call to Process.spawn;
   # see {Execution Options}[https://docs.ruby-lang.org/en/master/Process.html#module-Process-label-Execution+Options].
   #
-  # Note: The redirection options :in and :out will be ignored
-  # because popen2 sets up pipes for stdin and stdout.
+  # Note: Redirection options are managed by the method and should not be
+  # provided as they may be ignored or lead to errors.
   #
   # The single required argument is one of the following:
   #
@@ -461,8 +461,8 @@ module Open3
   # in the call to Process.spawn;
   # see {Execution Options}[https://docs.ruby-lang.org/en/master/Process.html#module-Process-label-Execution+Options].
   #
-  # Note: The redirection options :in and [:out, :err] will be ignored
-  # because popen2e sets up pipes for stdin and merged stdout/stderr.
+  # Note: Redirection options are managed by the method and should not be
+  # provided as they may be ignored or lead to errors.
   #
   # The single required argument is one of the following:
   #
@@ -602,8 +602,8 @@ module Open3
   # in the call to Open3.popen3;
   # see {Execution Options}[https://docs.ruby-lang.org/en/master/Process.html#module-Process-label-Execution+Options].
   #
-  # Note: The redirection options :in, :out, and :err will be ignored
-  # because capture3 manages stdin, stdout, and stderr internally.
+  # Note: Redirection options are managed by the method and should not be
+  # provided as they may be ignored or lead to errors.
   #
   # The hash +options+ is given;
   # two options have local effect in method Open3.capture3:
@@ -731,8 +731,8 @@ module Open3
   # in the call to Open3.popen3;
   # see {Execution Options}[https://docs.ruby-lang.org/en/master/Process.html#module-Process-label-Execution+Options].
   #
-  # Note: The redirection options :in and :out will be ignored
-  # because capture2 manages stdin and stdout internally.
+  # Note: Redirection options are managed by the method and should not be
+  # provided as they may be ignored or lead to errors.
   #
   # The hash +options+ is given;
   # two options have local effect in method Open3.capture2:
@@ -862,8 +862,8 @@ module Open3
   # in the call to Open3.popen3;
   # see {Execution Options}[https://docs.ruby-lang.org/en/master/Process.html#module-Process-label-Execution+Options].
   #
-  # Note: The redirection options :in and [:out, :err] will be ignored
-  # because capture2e manages stdin and merged stdout/stderr internally.
+  # Note: Redirection options are managed by the method and should not be
+  # provided as they may be ignored or lead to errors.
   #
   # The hash +options+ is given;
   # two options have local effect in method Open3.capture2e:
@@ -1028,9 +1028,8 @@ module Open3
   # in each call to Process.spawn;
   # see {Execution Options}[https://docs.ruby-lang.org/en/master/Process.html#module-Process-label-Execution+Options].
   #
-  # Note: The redirection options for the first process's stdin (:in)
-  # and the last process's stdout (:out) will be ignored
-  # because pipeline_rw sets up pipes for these.
+  # Note: Redirection options are managed by the method and should not be
+  # provided as they may be ignored or lead to errors.
   #
   # Each remaining argument in +cmds+ is one of:
   #
@@ -1121,8 +1120,8 @@ module Open3
   # in each call to Process.spawn;
   # see {Execution Options}[https://docs.ruby-lang.org/en/master/Process.html#module-Process-label-Execution+Options].
   #
-  # Note: The redirection option for the last process's stdout (:out)
-  # will be ignored because pipeline_r sets up a pipe for it.
+  # Note: Redirection options are managed by the method and should not be
+  # provided as they may be ignored or lead to errors.
   #
   # Each remaining argument in +cmds+ is one of:
   #
@@ -1214,8 +1213,8 @@ module Open3
   # in each call to Process.spawn;
   # see {Execution Options}[https://docs.ruby-lang.org/en/master/Process.html#module-Process-label-Execution+Options].
   #
-  # Note: The redirection option for the first process's stdin (:in)
-  # will be ignored because pipeline_w sets up a pipe for it.
+  # Note: Redirection options are managed by the method and should not be
+  # provided as they may be ignored or lead to errors.
   #
   # Each remaining argument in +cmds+ is one of:
   #
@@ -1293,8 +1292,8 @@ module Open3
   # in each call to Process.spawn;
   # see {Execution Options}[https://docs.ruby-lang.org/en/master/Process.html#module-Process-label-Execution+Options].
   #
-  # Note: All redirection options are honored because pipeline_start
-  # does not set up any pipes automatically.
+  # Note: Redirection options are managed by the method and should not be
+  # provided as they may be ignored or lead to errors.
   #
   # Each remaining argument in +cmds+ is one of:
   #
@@ -1358,8 +1357,8 @@ module Open3
   # in each call to Process.spawn;
   # see {Execution Options}[https://docs.ruby-lang.org/en/master/Process.html#module-Process-label-Execution+Options].
   #
-  # Note: Redirection options are generally honored, but pipes between
-  # commands are automatically managed.
+  # Note: Redirection options are managed by the method and should not be
+  # provided as they may be ignored or lead to errors.
   #
   # Each remaining argument in +cmds+ is one of:
   #
